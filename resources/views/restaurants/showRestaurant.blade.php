c39b06f9f1ec4b60c1131cf6bde91cee5ea4353a
alessandraspalla <alespalla93@icloud.com>
2023-09-15 15:31:59 +0200

    modifica header e show restaurant

--- resources/views/restaurants/showRestaurant.blade.php
+++ resources/views/restaurants/showRestaurant.blade.php
@@ -1,31 +1,45 @@
 @extends('layouts.app')
 @section('content')
 
-<h1 class='text-center bg-dark text-light py-2'>
-    DeliveBoo
-</h1>
-
-<div class="container d-flex justify-content-center">
-    <div class="row">
-        <a 
-        class="text-decoration-none card m-2 col-12" 
-        href="{{ route('show', $restaurant -> id)}}"
-        >
-            <div>
-                <h3 class="text-center">
-                    {{$restaurant -> activity_name}}
-                </h3>
-
-                <img src="{{ Storage::exists($restaurant->image_path) ? asset('storage/' . $restaurant->image_path) : $restaurant->image_path }}" width='200px' class="d-block m-auto my-2">
-
+<div id="showR" class="container">
+    <div class="row justify-content-between">
+        <div class="col-8 left">
+            <div 
+            class="card m-2 col-12" 
+            >
                 <div>
-                    <strong>Indirizzo:</strong> {{$restaurant -> address}}
-                </div>
-                <div>
-                    <strong>Tel:</strong> {{$restaurant -> mobile_phone}}
+                    <img src="{{ Storage::exists($restaurant->image_path) ? asset('storage/' . $restaurant->image_path) : $restaurant->image_path }}" width='200px' class="d-block m-auto my-2">
                 </div>
             </div>
-        </a>
+        </div>
+        <div class="col-3 right">
+            <h3>
+                {{$restaurant -> activity_name}}
+            </h3>
+            <div>
+                <i class="fa-solid fa-map-location-dot"></i>
+                <strong>Indirizzo: </strong>
+                {{$restaurant -> address}}
+            </div>
+            <div>
+                <i class="fa-solid fa-mobile-screen-button"></i>
+                <strong>Tel: </strong>
+                {{$restaurant -> mobile_phone}}
+            </div>
+            <div>
+                <i class="fa-solid fa-id-card"></i>
+                <strong>P.Iva: </strong>
+                {{$restaurant -> vat}}
+            </div>
+            <div>
+                <i class="fa-solid fa-utensils"></i>
+                <strong>
+                    <a href="{{ route('show', $restaurant -> id)}}" class="text-black">
+                        Menù
+                    </a>
+                </strong>
+            </div>
+        </div>
     </div>
 </div>
 
